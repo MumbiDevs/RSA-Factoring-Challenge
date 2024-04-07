bfac1210c218c0f62d365430ebd0a0b1eb37048a
MumbiDevs <113234080+MumbiDevs@users.noreply.github.com>
2024-04-07 13:21:59 +0300

    Create factores.py

---
+++ factors/factores.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+import sys
+
+def factorize_number(number):
+    flag = True
+    divisor = 2
+    while flag:
+        remainder = number % divisor
+        if remainder == 0:
+            counter = number // divisor
+            print(f"{number}={counter}*{divisor}")
+            flag = False
+        divisor += 1
+
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: ./factorize_numbers.py <file>")
+        sys.exit(1)
+
+    input_file = sys.argv[1]
+
+    with open(input_file, 'r') as file:
+        for line in file:
+            number = int(line.strip())
+            factorize_number(number)
+
+if __name__ == "__main__":
+    main()
